33d77042c3256759e0725a57ff6474305c767e09
Kinga Nowakowska <knowakowska@macbook-pro-kinga.home>
2025-10-06 15:08:03 +0200

    cookies investigation

--- src/config/fastify/fastify.ts
+++ src/config/fastify/fastify.ts
@@ -21,8 +21,8 @@ fastify.register(fastifyCookie, {
     expires: new Date(Date.now() + 60 * 60 * 1000), // 1 hour
     httpOnly: true,
     path: "/",
-    secure: process.env.NODE_ENV === "production",
-    sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+    secure: true,
+    sameSite: "none",
   },
 });
 
