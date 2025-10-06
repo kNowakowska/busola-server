8467493a4f13d54f34f26a8c382f6d1ec8e5a524
Kinga Nowakowska <knowakowska@macbook-pro-kinga.home>
2025-10-06 16:03:31 +0200

    cookies investigation

--- src/controllers/auth/signIn.controller.ts
+++ src/controllers/auth/signIn.controller.ts
@@ -46,7 +46,7 @@ export async function signIn(
           sameSite: "none",
           path: "/",
           maxAge: 60 * 60, // 1 hour
-          domain: process.env.VERCEL_FRONTEND_URL!,
+          domain: new URL(process.env.VERCEL_FRONTEND_URL!).hostname,
         })
         .setCookie("refresh_token", refreshToken, {
           httpOnly: true,
@@ -54,7 +54,7 @@ export async function signIn(
           sameSite: "none",
           path: "/",
           maxAge: 60 * 60 * 24 * 7, // 7 days
-          domain: process.env.VERCEL_FRONTEND_URL!,
+          domain: new URL(process.env.VERCEL_FRONTEND_URL!).hostname,
         })
         .status(200)
         .send({ shouldResetPassword: false });
