be4268fc04e9becc44cdf6c4bc733e88d4bb38a2
Kinga Nowakowska <k.nowakowska.013@gmail.com>
2025-12-30 17:40:52 +0100

    Attempt to fix integration

--- src/controllers/auth/signIn.controller.ts
+++ src/controllers/auth/signIn.controller.ts
@@ -46,6 +46,7 @@ export async function signIn(
           sameSite: process.env.NODE_ENV === "dev" ? "lax" : "none",
           path: "/",
           maxAge: 60 * 60, // 1 hour
+          domain: process.env.NODE_ENV === "dev" ? undefined : ".knowakowska.tech",
         })
         .setCookie("refresh_token", refreshToken, {
           httpOnly: true,
@@ -53,6 +54,7 @@ export async function signIn(
           sameSite: process.env.NODE_ENV === "dev" ? "lax" : "none",
           path: "/",
           maxAge: 60 * 60 * 24 * 7, // 7 days
+          domain: process.env.NODE_ENV === "dev" ? undefined : ".knowakowska.tech",
         })
         .status(200)
         .send({ shouldResetPassword: false });
