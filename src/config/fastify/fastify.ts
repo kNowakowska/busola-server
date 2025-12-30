d623c2c9ada6c6789d95a3a51fb1db35446ced1a
Kinga Nowakowska <k.nowakowska.013@gmail.com>
2025-12-30 15:46:55 +0100

    Attempt to fix integration

--- src/config/fastify/fastify.ts
+++ src/config/fastify/fastify.ts
@@ -42,7 +42,7 @@ fastify.register(fastifyCookie, {
     path: "/",
     secure: process.env.NODE_ENV === "prod",
     sameSite: "lax",
-    domain: process.env.FRONTEND_URL?.split("://")[1],
+    domain: "onrender.com",
   },
 });
 
